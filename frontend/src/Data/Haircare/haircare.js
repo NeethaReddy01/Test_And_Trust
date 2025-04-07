ecce88e02fb3835f705b7f1cc7f9575c934fba93
BShivaGanesh <boggarapushivaganesh@gmail.com>
2025-04-07 14:59:13 +0530

    updated package.json

---
+++ frontend/src/Data/Haircare/haircare.js
@@ -0,0 +1,51 @@
+const haircare = [
+    {
+        
+        "image": "https://cdn.fcglcdn.com/brainbees/images/products/583x720/18128609a.webp",
+        "brand": "Rosa",
+        "title": "Rosa Green Apple shampoo ",
+        "color": "Transparent",
+        "selling_price": "₹10",
+        "price": "₹10",
+        "disscount": "0% off",
+        "size": "20ml"  
+    },
+
+    {
+        
+        "image": "https://images.mamaearth.in/catalog/product/3/_/3_11.jpg?format=auto&height=600",
+        "brand": "Mamaearth",
+        "title": "Rosemary Anti-Hair Fall Shampoo",
+        "color": "Transparent",
+        "selling_price": "₹90",
+        "price": "₹99",
+        "disscount": "10% off",
+        "size": "20ml"  
+    },
+
+    {
+        
+        "image": "https://images.mamaearth.in/catalog/product/3/_/3_1_1_1.png?format=auto&height=600",
+        "brand": "Mamaearth",
+        "title": "Rosemary Hair Growth Oil",
+        "color": "Transparent",
+        "selling_price": "₹90",
+        "price": "₹99",
+        "disscount": "10% off",
+        "size": "20ml"  
+    },
+
+    {
+        
+        "image": "https://images.mamaearth.in/catalog/product/3/_/3_1_1_1.png?format=auto&height=600",
+        "brand": "Mamaearth",
+        "title": "Rosemary Hair Growth Oil",
+        "color": "Transparent",
+        "selling_price": "₹90",
+        "price": "₹99",
+        "disscount": "10% off",
+        "size": "20ml"  
+    },
+
+    
+]
