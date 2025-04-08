59f2dab3d53a5960b70d04eedbc2a4bf01d421a8
NeethaReddy01 <110006682+NeethaReddy01@users.noreply.github.com>
2025-04-08 15:50:12 +0530

    Added AuthModal, Login, Register Forms

--- frontend/src/Data/Makeup/MakeupProducts.js
+++ frontend/src/Data/Makeup/MakeupProducts.js
@@ -100,7 +100,204 @@ const MakepupProducts = [
         "size":"1ml"
     },
     {
-        
+        "image": "https://5.imimg.com/data5/SELLER/Default/2021/3/AV/NN/ZT/2197889/coat-me-bonjour-paris-nano-pearl-powder-s02-500x500.jpg",
+        "brand": "Coat Me Bonjour Paris",
+        "title": "Photo Match Primer",
+        "color": "light cream",
+        "selling_price": "₹117",
+        "price": "₹195",
+        "disscount": "40% off",
+        "size":"4gm"
+    },
+    {
+        "image": "https://www.bing.com/th/id/OIP.7Xn5nMu56PpplAlvrgKWpAHaJr?w=150&h=196&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+        "brand": "Million Colors",
+        "title": "Put It On Strawberry Lip Balm",
+        "color": "orange",
+        "selling_price": "₹109",
+        "price": "₹149",
+        "disscount": "27% off",
+        "size":"5gm"
+    },
+    {
+        "image": "https://th.bing.com/th/id/OIP.VcQpRbYP16IliNLxmLjQEQHaHa?pid=ImgDet&w=184&h=184&c=7&dpr=1.3",
+        "brand": "MILA BEAUTÉ",
+        "title": "Wink Artist Gel Liner 2in1 With Brush Waterproof Eyeliner Upto 36hrs - Black",
+        "color": "black",
+        "selling_price": "₹172",
+        "price": "₹249",
+        "disscount": "31% off",
+        "size":"10gm"
+    },
+    {
+        "image": "https://glamourusus.com/cdn/shop/products/glamour-us-beauty-creations-flawless-stay-hydrating-face-primer-makeup-phs01-147378.jpg?v=1672485798&width=1500",
+        "brand": "MILA BEAUTÉ",
+        "title": "The Flawless Makeup Primer Oil-Free Smooth & Blur",
+        "color": "Transparent",
+        "selling_price": "146₹",
+        "price": "₹275",
+        "disscount": "47% off",
+        "size":"30ml"
+    },
+    {
+        "image": "https://www.reneecosmetics.in/cdn/shop/files/RENEE_DCFilter_Dark_ListingPI_01_edb360de-21f9-447c-a94a-607f6988347d.jpg?v=1742484985",
+        "brand": "RENEE",
+        "title": "DC Filter - Dark",
+        "color": "cream",
+        "selling_price": "₹315",
+        "price": "₹450",
+        "disscount": "30% off",
+        "size":"10gm"
+    },
+    {
+        "image": "https://i5.walmartimages.com/seo/Relove-by-Revolution-Pore-Vanish-Face-Primer_4d98b83c-3ba2-4b41-8dd1-5937efaa45ee.0c8aa105b19a64d39e47a5bcfb8703d7.jpeg",
+        "brand": "Makeup Revolution",
+        "title": "Revolution Relove Pore Vanish Primer",
+        "color": "white",
+        "selling_price": "₹215",
+        "price": "₹499",
+        "disscount": "57% off",
+        "size":"12ml"
+    },
+    {
+        "image": "https://images-static.nykaa.com/media/catalog/product/5/8/5806cb2676538924660_5.jpg",
+        "brand": "Iris Cosmetics",
+        "title": "All That Eye Need Smudge Proof Eyeliner",
+        "color": "black",
+        "selling_price": "₹232",
+        "price": "₹799",
+        "disscount": "71% off",
+        "size":"1ml"
+    },
+    {
+        "image": "https://cdn.fcglcdn.com/brainbees/images/products/583x720/19907621a.webp",
+        "brand": "Faces Canada",
+        "title": "Comfy Matte Wow Liquid Lipstick Choco Couture",
+        "color": "brown",
+        "selling_price": "₹221",
+        "price": "₹299",
+        "disscount": "26% off",
+        "size":"5ml"
+    },
+    {
+        "image": "https://www.netmeds.com/images/product-v1/600x600/1142525/renee_stars_face_and_body_glitter_rose_gold_5_gm_757105_3_0.jpg",
+        "brand": "RENEE",
+        "title": "Stars - Rose Gold",
+        "color": "red",
+        "selling_price": "₹172",
+        "price": "₹399",
+        "disscount": "57% off",
+        "size":"5g"
+    },
+    {
+        "image": "https://m.media-amazon.com/images/I/51ZVtvs+ozL._SX522_.jpg",
+        "brand": "Mars Cosmetics",
+        "title": "Natural Wet Wipes - Ice",
+        "color": "off white",
+        "selling_price": "₹79",
+        "price": "₹100",
+        "disscount": "21% off",
+        "size":"25 wipes"
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
+    },
+    {
+        "image": "",
+        "brand": "",
+        "title": "",
+        "color": "",
+        "selling_price": "₹",
+        "price": "₹",
+        "disscount": "% off",
+        "size":""
     }
 ];
 export default MakepupProducts;
