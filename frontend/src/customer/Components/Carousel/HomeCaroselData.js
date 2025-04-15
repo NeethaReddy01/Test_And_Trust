cdf01bb73e0a63daf2acae228a91d6998aa559b5
NeethaReddy01 <110006682+NeethaReddy01@users.noreply.github.com>
2025-04-15 17:21:49 +0530

    Updated Pages

--- frontend/src/customer/Components/Carousel/HomeCaroselData.js
+++ frontend/src/customer/Components/Carousel/HomeCaroselData.js
@@ -1,14 +1,14 @@
 export const homeCarouselData=[
     {
-        image:"https://aqualogica.in/cdn/shop/files/Clear-LaningPage-New-Desktop.jpg?v=1710501156",
+        image:"https://reana.pk/cdn/shop/articles/maxresdefault_a08a852f-7595-4c35-9a64-256782f3280c.jpg?v=2016984225&width=799",
         path:""
     },
     {
-        image:"https://m.media-amazon.com/images/S/aplus-media/sc/9983614d-3409-4b0a-8431-b7f13f208b2c.__CR0,0,1464,600_PT0_SX1464_V1___.jpg",
+        image:"https://m.media-amazon.com/images/S/aplus-media/sc/9983614d-3409-4b0a-8431-b7f13f208b2c.__CR0,0,1464,600_PT0_SX1464_V1__1797x499.jpg",
         path:""
     },
     {
-        image:"https://images-static.nykaa.com/uploads/10ca36ad-347f-43a3-b3eb-3a43133dba0f.jpg?tr=cm-pad_resize,w-1800",
+        image:"https://vanitywagon.in/cdn/shop/collections/Dot-_-Key-Top-Banner_1797x499.jpg?v=1665826237",
         path:""
     },
     {
