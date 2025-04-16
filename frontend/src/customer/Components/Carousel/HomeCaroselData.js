f2bb35392ef2b39aa040f1e5aebd3b96620f9690
NeethaReddy01 <110006682+NeethaReddy01@users.noreply.github.com>
2025-04-16 10:58:00 +0530

    updated homecarosel

--- frontend/src/customer/Components/Carousel/HomeCaroselData.js
+++ frontend/src/customer/Components/Carousel/HomeCaroselData.js
@@ -1,10 +1,10 @@
 export const homeCarouselData=[
     {
-        image:"https://reana.pk/cdn/shop/articles/maxresdefault_a08a852f-7595-4c35-9a64-256782f3280c.jpg?v=2016984225&width=799",
+        image:"https://plumgoodness.com/cdn/shop/files/HP_Desktop_Nia_FW_150_ml_1920x500px.jpg?v=1744367176&width=1920",
         path:""
     },
     {
-        image:"https://m.media-amazon.com/images/S/aplus-media/sc/9983614d-3409-4b0a-8431-b7f13f208b2c.__CR0,0,1464,600_PT0_SX1464_V1__1797x499.jpg",
+        image:"https://plumgoodness.com/cdn/shop/files/Desktop_Banners_copy_1.jpg?v=1744721260&width=1920",
         path:""
     },
     {
