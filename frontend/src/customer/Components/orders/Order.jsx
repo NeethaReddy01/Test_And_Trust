e89ec4f818952ba1065355c15539260e628ee04b
BShivaGanesh <boggarapushivaganesh@gmail.com>
2025-04-16 10:56:32 +0530

    updated order page

--- frontend/src/customer/Components/orders/Order.jsx
+++ frontend/src/customer/Components/orders/Order.jsx
@@ -24,7 +24,7 @@ const Order = () => {
     <Box className="px-10">
       <Grid container spacing={0} sx={{ justifyContent: "space-between" }}>
         <Grid item xs={2.5} className="">
-          <div className="h-auto shadow-lg bg-white border p-5 sticky top-5">
+          {/* <div className="h-auto shadow-lg bg-white border p-5 sticky top-5">
             <h1 className="font-bold text-lg">Filters</h1>
             <div className="space-y-4 mt-10">
               <h1 className="font-semibold">ORDER STATUS</h1>
@@ -47,7 +47,7 @@ const Order = () => {
                 </div>
               ))}
             </div>
-          </div>
+          </div> */}
         </Grid>
         <Grid item xs={9}>
           <Box className="space-y-5 ">
