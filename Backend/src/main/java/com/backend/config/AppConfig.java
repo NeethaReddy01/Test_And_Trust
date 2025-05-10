3f5429ca81377feb3a4b65746c5eaee1b6d90cce
kaushal8333 <krishnakaushald09@gmail.com>
2025-05-10 20:04:03 +0530

    added docker files and changes in properties

--- Backend/src/main/java/com/backend/config/AppConfig.java
+++ Backend/src/main/java/com/backend/config/AppConfig.java
@@ -44,7 +44,7 @@ public class AppConfig {
 								"http://localhost:3000", 
 								"http://localhost:4000",
 								"http://localhost:4200",
-								"https://testandtrust.netlify.app/*"
+								"https://testandtrust.netlify.app/"
 							)
 						);
 						//cfg.setAllowedMethods(Arrays.asList("GET", "POST","DELETE","PUT"));
